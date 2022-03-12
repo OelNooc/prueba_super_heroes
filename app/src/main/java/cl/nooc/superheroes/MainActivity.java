67d9d4b0c95043354e3f74bd27c32547f5251f15
Leonardo Catejo <leo.catejoa@gmail.com>
2022-03-12 20:17:41 -0300

    Add media player and bgm

--- app/src/main/java/cl/nooc/superheroes/MainActivity.java
+++ app/src/main/java/cl/nooc/superheroes/MainActivity.java
@@ -2,13 +2,24 @@ package cl.nooc.superheroes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
+import cl.nooc.superheroes.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mp = MediaPlayer.create(this, R.raw.marvel_intro);
+        mp.setLooping(true);
+        mp.start();
     }
 }
