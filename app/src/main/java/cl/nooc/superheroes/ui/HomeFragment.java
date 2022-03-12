0107923fb58c65e4fbcc589794de69b42200d3d4
Leonardo Catejo <leo.catejoa@gmail.com>
2022-03-12 20:12:01 -0300

    Add ui design pt1 and nav

---
+++ app/src/main/java/cl/nooc/superheroes/ui/HomeFragment.java
@@ -0,0 +1,24 @@
+package cl.nooc.superheroes.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cl.nooc.superheroes.R;
+import cl.nooc.superheroes.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+}
