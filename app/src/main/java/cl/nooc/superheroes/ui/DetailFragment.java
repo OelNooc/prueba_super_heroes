6381127dc42aa4c774b876383dd7c1f1128e3987
Leonardo Catejo <leo.catejoa@gmail.com>
2022-03-17 12:05:30 -0300

    Add viewModel to DetailFragment

--- app/src/main/java/cl/nooc/superheroes/ui/DetailFragment.java
+++ app/src/main/java/cl/nooc/superheroes/ui/DetailFragment.java
@@ -3,19 +3,38 @@ package cl.nooc.superheroes.ui;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.squareup.picasso.Picasso;
+
 import cl.nooc.superheroes.R;
+import cl.nooc.superheroes.databinding.FragmentDetailBinding;
+import cl.nooc.superheroes.viewmodel.HeroViewModel;
 
 public class DetailFragment extends Fragment {
+
+    private FragmentDetailBinding binding;
+    private HeroViewModel viewModel;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        binding = FragmentDetailBinding.inflate(inflater, container, false);
+
+        binding.setLifecycleOwner(getViewLifecycleOwner());
+
+        viewModel = new ViewModelProvider(getActivity()).get(HeroViewModel.class);
+
+        viewModel.getDetalle().observe(getViewLifecycleOwner(), superRespuestaItem -> {
+            binding.setDetalle(superRespuestaItem);
+
+            Picasso.get().load(superRespuestaItem.getImages().getSm()).into(binding.ivDetail);
+        });
+
+        return binding.getRoot();
     }
 }
