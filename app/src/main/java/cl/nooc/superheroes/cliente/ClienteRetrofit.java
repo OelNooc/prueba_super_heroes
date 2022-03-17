a3e83f5e07e00b208632c4dc95e0dda9f99f1906
Leonardo Catejo <leo.catejoa@gmail.com>
2022-03-17 10:37:02 -0300

    Fix ClienteRetrofit

--- app/src/main/java/cl/nooc/superheroes/cliente/ClienteRetrofit.java
+++ app/src/main/java/cl/nooc/superheroes/cliente/ClienteRetrofit.java
@@ -7,15 +7,16 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ClienteRetrofit {
 
     private static Retrofit instance;
+    public final static String BASE_URL = "https://akabab.github.io/superhero-api/api/";
 
     private ClienteRetrofit() {}
 
-    public static SuperServicio getInstance()
+    public static SuperServicio getInstance(String url)
     {
         if(instance == null)
         {
             instance = new Retrofit.Builder()
-                    .baseUrl("https://akabab.github.io/superhero-api/api/")
+                    .baseUrl(url)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
