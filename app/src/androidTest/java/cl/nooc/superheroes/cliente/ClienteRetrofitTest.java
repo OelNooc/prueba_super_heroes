f22984cf5e6006ff5d1dac0d6363fd369a46bd65
Leonardo Catejo <leo.catejoa@gmail.com>
2022-03-21 09:39:53 -0300

    Add ClienteRetrofitTest and HeroViewModelTest

---
+++ app/src/androidTest/java/cl/nooc/superheroes/cliente/ClienteRetrofitTest.java
@@ -0,0 +1,22 @@
+package cl.nooc.superheroes.cliente;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ClienteRetrofitTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getInstance() {
+    }
+}
